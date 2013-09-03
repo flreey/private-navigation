40018627d19ad2b7c68de3d5f2bf42e141fa1185
flreey <flreey@gmail.com>
2013-09-03 23:39:37 +0800

    add button

--- scripts/initdb.py
+++ scripts/initdb.py
@@ -16,18 +16,18 @@ with app.app_context():
 	Base.metadata.drop_all(bind=engine)
 	init_db()
 
-	#u = User(name='flreey', email='flreey@gmail.com', password='123456').insert()
+	u = User(name='flreey', email='flreey@gmail.com', password='123456').insert()
 
-	#data = {'Blog': ['InfoQ', 'ItEye', 'CSDN', 'Solidot', '技术博客'],
-	#'Design': ['Mile', '淘宝UED' * 2, '腾讯UX'], 'Launguage': ['Python', 'Java',
-		#'Erlang', 'Haskel', 'Jquery', 'Css'], 'Tools': ['Vim', 'HHKB',
-			#'Mockup', 'LiveReload' * 2], 'Apple' * 2: ['IOS',
-				#'IPHONE', 'IPad', 'MacOS']}
+	data = {'Blog': ['InfoQ', 'ItEye', 'CSDN', 'Solidot', '技术博客'],
+	'Design': ['Mile', '淘宝UED' * 2, '腾讯UX'], 'Launguage': ['Python', 'Java',
+		'Erlang', 'Haskel', 'Jquery', 'Css'], 'Tools': ['Vim', 'HHKB',
+			'Mockup', 'LiveReload' * 2], 'Apple' * 2: ['IOS',
+				'IPHONE', 'IPad', 'MacOS']}
 
-	#for i in range(3):
-		#for k, v in data.copy().items():
-			#data[k+'1'] = v
+	for i in range(3):
+		for k, v in data.copy().items():
+			data[k+'1'] = v
 
-	#for cat, websites in data.items():
-		#c = Category(user_id=u.id, title=cat).insert()
-		#[WebSite(title=w, url='#', category_id=c.id).insert() for w in websites]
+	for cat, websites in data.items():
+		c = Category(user_id=u.id, title=cat).insert()
+		[WebSite(title=w, url='#', category_id=c.id).insert() for w in websites]
